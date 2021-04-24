b7cc72074282603f3138803d0c4a9afed1ded92c
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-24 14:44:23 +0530

    Added comparision like  greater or less than for both lines

--- lineComp.java
+++ lineComp.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.lang.Math;
 
 public class lineComp {
+
 	public static void main(String[] args){
 
 		System.out.println("Welcome to Line Comparison Computation Program");
@@ -19,25 +20,25 @@ public class lineComp {
 
 		System.out.println("Enter the Values for second line!!");
 		System.out.println("Enter value of m1:-");
-      double m1= sc.nextInt();
-      System.out.println("Enter value of n1:-");
-      double n1= sc.nextInt();
-      System.out.println("Enter value of m2:-");
-      double m2= sc.nextInt();
-      System.out.println("Enter value of n2:-");
-      double n2= sc.nextInt();
+		double m1= sc.nextInt();
+		System.out.println("Enter value of n1:-");
+		double n1= sc.nextInt();
+		System.out.println("Enter value of m2:-");
+		double m2= sc.nextInt();
+		System.out.println("Enter value of n2:-");
+		double n2= sc.nextInt();
 
 		double len1 = Math.sqrt((Math.pow((x2-x1),2)) + (Math.pow((y2-y1),2)));
-      System.out.println("The Length of Line 1 is:-" + len1);
+		System.out.println("The Length of Line 1 is:-" + len1);
 
-      double len2 = Math.sqrt((Math.pow((m2-m1),2)) + (Math.pow((n2-n1),2)));
-      System.out.println("The Length of Line 2 is:-" + len2);
+		double len2 = Math.sqrt((Math.pow((m2-m1),2)) + (Math.pow((n2-n1),2)));
+		System.out.println("The Length of Line 2 is:-" + len2);
 
 		if (len2 == len1)
-      	System.out.println("The 2 Lines are Equal!!");
-      else if (len1 > len2)
+			System.out.println("The 2 Lines are Equal!!");
+		else if (len1 > len2)
 			System.out.println("Length of Line 1 is Greater than Length of Line 2");
 		else if (len1 < len2)
-         System.out.println("Length of Line 1 is Smaller than Length of Line 2");
+			System.out.println("Length of Line 1 is Smaller than Length of Line 2");
 	}
 }
